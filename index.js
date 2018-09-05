9bbac91cbef1fc2e679b7e222526554a8ba1f600
Renato Menegasso <renato.menegasso@bidu.com.br>
2018-09-05 11:06:16 -0300

    Load errorCount data

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const auth = require('./lib/authorize');
+const gsc = require('./lib/gsc');
 
 exports.handler = async (event, context, callback) => {
   let authData = await auth();
-  console.log(authData);
+
+  let data = {},
+      sites = process.env.SITE_LIST.split(',');
+
+  for(let i = 0; i < sites.length; i ++) {
+    let errorCounts = await gsc.urlCrawlErrorsCounts(authData.client, sites[i]);
+
+    let result = {
+      site: sites[i],
+      errorsCount: errorCounts
+    };
+
+    data[sites[i]] = result;
+  };
+
+  console.log(JSON.stringify(data, null, 2));
 };
