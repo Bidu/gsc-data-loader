9242e5920c516430bf6ca4ab099104ced40e480b
carloosamarante <amarante.carlos@hotmail.com>
2018-09-06 13:20:49 -0300

    Load date lib and call functions

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const auth = require('./lib/authorize');
 const gsc = require('./lib/gsc');
+const date = require('./lib/date');
 
 exports.handler = async (event, context, callback) => {
   let authData = await auth();
 
   let data = {},
-      sites = process.env.SITE_LIST.split(','),
-      today = new Date();
+      sites = process.env.SITE_LIST.split(',');
 
   for(let i = 0; i < sites.length; i ++) {
-    let errorCounts = await gsc.urlCrawlErrorsCounts(authData.client, sites[i]),
-        searchAnalytics = await gsc.searchAnalytics(authData.client, sites[i]);
+    let errorCounts = await gsc.urlCrawlErrorsCounts(authData.client, sites[i]);
+        searchAnalytics = await gsc.searchAnalytics(authData.client, sites[i], date.start(), date.end() );
 
     let result = {
       site: sites[i],
