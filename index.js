dda9fd0f8b67462797492d095d8d6896d58ac816
carloosamarante <amarante.carlos@hotmail.com>
2018-09-06 12:04:17 -0300

    Implement searchAnalytics api

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ exports.handler = async (event, context, callback) => {
   let authData = await auth();
 
   let data = {},
-      sites = process.env.SITE_LIST.split(',');
+      sites = process.env.SITE_LIST.split(','),
+      today = new Date();
 
   for(let i = 0; i < sites.length; i ++) {
-    let errorCounts = await gsc.urlCrawlErrorsCounts(authData.client, sites[i]);
+    let errorCounts = await gsc.urlCrawlErrorsCounts(authData.client, sites[i]),
+        searchAnalytics = await gsc.searchAnalytics(authData.client, sites[i]);
 
     let result = {
       site: sites[i],
-      errorsCount: errorCounts
+      errorsCount: errorCounts,
+      searchAnalytics: searchAnalytics.data
     };
-
     data[sites[i]] = result;
   };
 
   console.log(JSON.stringify(data, null, 2));
+
 };
