89260d2aa5a848c791acdcafb1f322fedcfdac4d
carloosamarante <amarante.carlos@hotmail.com>
2018-09-10 14:49:52 -0300

    Fix identation, call errorsamples function and refactor loops

--- index.js
+++ index.js
@@ -6,20 +6,34 @@ exports.handler = async (event, context, callback) => {
   let authData = await auth();
 
   let data = {},
-      sites = process.env.SITE_LIST.split(',');
+      sites = process.env.SITE_LIST.split(','),
+      categories = process.env.CATEGORIES.split(','),
+      platforms = process.env.PLATFORMS.split(',');
 
-  for(let i = 0; i < sites.length; i ++) {
-    let errorCounts = await gsc.urlCrawlErrorsCounts(authData.client, sites[i]),
-        searchAnalytics = await gsc.searchAnalytics(authData.client, sites[i], date.start(), date.end() ),
-        sitemaps = await gsc.sitemaps(authData.client, sites[i]);
+  for(site of sites) {
+    let errorCounts = await gsc.urlCrawlErrorsCounts(authData.client, site),
+        searchAnalytics = await gsc.searchAnalytics(authData.client, site, date.start(), date.end() ),
+        sitemaps = await gsc.sitemaps(authData.client, site),
+        errorsSamples = [];
+
+    for (let n = 0; n < categories.length; n ++) {
+      errorsSamples[n] = [];
+      for (platform of platforms) {
+        errorSample = await gsc.errorsSamples(authData.client, site, categories[n],
+          platform);
+        errorsSamples[n].push(errorSample.data);
+      }
+    }
 
     let result = {
-      site: sites[i],
+      site,
       errorsCount: errorCounts,
-      searchAnalytics: searchAnalytics,
-      sitemaps: sitemaps
+      searchAnalytics,
+      sitemaps,
+      errorsSamples,
+
     };
-    data[sites[i]] = result;
+    data[site] = result;
   };
 
   console.log(JSON.stringify(data, null, 2));
