ced6104e8e82d3b7709423686b13bd20946d0e76
Carlos Amarante <amarante.carlos@hotmail.com>
2018-12-06 10:46:02 -0200

    Simplify code and put just one function

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const gsc = require('./lib/gsc');
 const date = require('./lib/date');
 const bigquery = require('./lib/bigquery');
 
-searchAnalyticsQueries = async (req, res) => {
-  const authData =  await auth(),
-      AnalyticsRows = [],
-      searchAnalyticsQueries = [],
-      sites = process.env.SITE_LIST.split(',');
+exports.gscDataLoader = async (req, res) => {
+  let authData = await auth();
+
+  const data = {},
+        errorsCountRows = [],
+        searchAnalyticsRows = [],
+        sitemapsRows = [],
+        errorSamplesRows = [],
+        analyticsRows = [],
+        searchAnalyticsQueries = [],
+        sites = process.env.SITE_LIST.split(',');
 
   for(site of sites) {
-    AnalyticsRows.push(await gsc.searchAnalytics(authData.client, site, date.threeDaysAgo(), date.currentDay(), ["query"]));
-    AnalyticsRows.forEach(row => {
+    errorsCountRows.push(await gsc.urlCrawlErrorsCounts(authData.client, site));
+    searchAnalyticsRows.push(await gsc.searchAnalytics(authData.client, site, date.threeDaysAgo(), date.currentDay()));
+    sitemapsRows.push(await gsc.sitemaps(authData.client, site));
+    errorSamplesRows.push(await gsc.errorsSamples(authData.client, site));
+    analyticsRows.push(await gsc.searchAnalytics(authData.client, site, date.threeDaysAgo(), date.currentDay(), ["query"]));
+    analyticsRows.forEach(row => {
       row.data.forEach(fields => {
         searchAnalyticsQueries.push({
           siteUrl: site,
@@ -24,31 +34,11 @@ searchAnalyticsQueries = async (req, res) => {
         });
       });
     });
-  }
-  await bigquery.insert('search_analytics_queries', searchAnalyticsQueries);
-  res.send();
-}
-
-exports.gscDataLoader = async (req, res) => {
-  let authData = await auth();
-
-  let data = {},
-      errorsCountRows = [],
-      searchAnalyticsRows = [],
-      sitemapsRows = [],
-      errorSamplesRows = [],
-      sites = process.env.SITE_LIST.split(',');
-
-  for(site of sites) {
-    errorsCountRows.push(await gsc.urlCrawlErrorsCounts(authData.client, site));
-    searchAnalyticsRows.push(await gsc.searchAnalytics(authData.client, site, date.threeDaysAgo(), date.currentDay()));
-    sitemapsRows.push(await gsc.sitemaps(authData.client, site));
-    errorSamplesRows.push(await gsc.errorsSamples(authData.client, site));
   };
   await bigquery.insert('errors_count', errorsCountRows);
   await bigquery.insert('search_analytics', searchAnalyticsRows);
   await bigquery.insert('sitemaps', sitemapsRows);
   await bigquery.insert('errors_samples', errorSamplesRows);
-
+  await bigquery.insert('search_analytics_queries', searchAnalyticsQueries);
   res.send();
 };
