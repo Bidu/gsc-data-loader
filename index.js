f52b40d46108214a2e9a65235c82bf7a0ec3e34b
Carlos Amarante <amarante.carlos@hotmail.com>
2018-12-10 16:16:58 -0200

    Create external function to handle analyticsQueries

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const date = require('./lib/date');
 const bigquery = require('./lib/bigquery');
 
 exports.gscDataLoader = async (req, res) => {
-  let authData = await auth();
+  let authData = await auth(),
+      searchAnalyticsQueries = [],
+      searchAnalyticsResult = [];
 
   const data = {},
         errorsCountRows = [],
         searchAnalyticsRows = [],
         sitemapsRows = [],
         errorSamplesRows = [],
-        analyticsRows = [],
-        searchAnalyticsQueries = [],
         sites = process.env.SITE_LIST.split(',');
 
   for(site of sites) {
@@ -21,26 +21,13 @@ exports.gscDataLoader = async (req, res) => {
     sitemapsRows.push(await gsc.sitemaps(authData.client, site));
     errorSamplesRows.push(await gsc.errorsSamples(authData.client, site));
 
-    let searchAnalyticsResult = await gsc.searchAnalytics(authData.client, site, date.threeDaysAgo(), date.currentDay(), ["query"]);
-    analyticsRows.push(searchAnalyticsResult);
-    analyticsRows.forEach(row => {
-      row.data.forEach(fields => {
-        searchAnalyticsQueries.push({
-          siteUrl: site,
-          createdAt: new Date(),
-          key: fields.keys[0],
-          clicks: fields.clicks,
-          impressions: fields.impressions,
-          ctr: fields.ctr,
-          position: fields.position
-        });
-      });
-    });
+    searchAnalyticsResult = await gsc.searchAnalytics(authData.client, site, date.threeDaysAgo(), date.currentDay(), ["query"]);
+    searchAnalyticsQueries.push(await gsc.searchAnalyticsQueries(searchAnalyticsResult, site));
   };
   await bigquery.insert('errors_count', errorsCountRows);
   await bigquery.insert('search_analytics', searchAnalyticsRows);
   await bigquery.insert('sitemaps', sitemapsRows);
   await bigquery.insert('errors_samples', errorSamplesRows);
-  await bigquery.insert('search_analytics_queries', searchAnalyticsQueries);
+  await bigquery.insert('search_analytics_queries', searchAnalyticsQueries.reduce((result, current) => result.concat(current)));
   res.send();
 };
